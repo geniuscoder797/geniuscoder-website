632e9efa384e906ed59558f9d306d23f8944194b
geniuscoder797 <149076817+geniuscoder797@users.noreply.github.com>
2023-10-27 06:42:39 +0100

    Add files via upload

---
+++ post-holder.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+
+$email_from = 'info@geniuscoder.com';
+
+$email_subject = 'New Form Submission';
+
+
+$email_body = "User name: $name.\n".
+               "User name: $visitor_email.\n".
+                "User name: $subject.\n".
+                 "User name: $message.\n";
+
+
+$to = 'afolabiabdulkabirakanbi@gmail.com';
+
+$headers = "From: $email_from \r\n";
+$headers .= "Reply_TO: $visitor_email\r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("location: contact.html");
+?>
